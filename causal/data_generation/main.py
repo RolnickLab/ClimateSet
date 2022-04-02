d463938ca6b435b01fe568a968455aa7f9354e94
kurowasan <philippebrouillard@gmail.com>
2022-04-02 18:23:19 -0400

    Add data generating process with spatial clustering

---
+++ causal/data_generation/main.py
@@ -0,0 +1,66 @@
+import argparse
+import os
+import torch
+
+import numpy as np
+from data_generation import DataGenerator
+from plot import plot_adjacency_graphs, plot_adjacency_w, plot_x, plot_z
+
+
+def main(hp):
+    """
+    :param hp: object containing hyperparameter values
+    """
+    # Control as much randomness as possible
+    torch.manual_seed(hp.random_seed)
+    np.random.seed(hp.random_seed)
+
+    generator = DataGenerator(args)
+    data = generator.generate()
+
+    plot_adjacency_graphs(generator.G, hp.exp_path)
+    plot_adjacency_w(generator.w, hp.exp_path)
+    plot_x(generator.X.detach().numpy(), hp.exp_path)
+    plot_z(generator.Z.detach().numpy(), hp.exp_path)
+
+    return data
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=" Code use to generate synthetic data to test the end-to-end clustering idea.")
+
+    parser.add_argument("--exp-path", type=str, default="../causal_climate_exp/exp",
+                        help="Path to experiments")
+    parser.add_argument("--random-seed", type=int, default=2,
+                       help="Random seed used for torch and numpy")
+
+    # Dataset properties
+    parser.add_argument("--n", type=int, default=1,
+                       help="Number of time-series")
+    parser.add_argument("-t", "--num-timesteps", type=int, default=10,
+                       help="Number of timesteps in total")
+    parser.add_argument("-d", "--num-features", type=int, default=2,
+                       help="Number of features")
+    parser.add_argument("-g", "--num-gridcells", type=int, default=10,
+                       help="Number of gridcells")
+    parser.add_argument("-k", "--num-clusters", type=int, default=3,
+                       help="Number of clusters")
+    parser.add_argument("-p", "--prob", type=float, default=0.3,
+                       help="Probability of an edge in the causal graphs")
+
+    parser.add_argument("--timewindow", type=int, default=3,
+                       help="Number of previous timestep that interacts with a timestep t")
+
+    # Neural network (NN) architecture
+    parser.add_argument("--num-layers", type=int, default=1,
+                       help="Number of layers in NN")
+    parser.add_argument("--num-hidden", type=int, default=4,
+                       help="Number of hidden units in NN")
+    parser.add_argument("--non-linearity", type=str, default="relu",
+                       help="Type of non-linearity used in the NN")
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.exp_path):
+        os.makedirs(args.exp_path)
+
+    main(args)
---
+++ causal/data_generation/plot.py
@@ -0,0 +1,55 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def plot_adjacency_graphs(g, path):
+    n_graph = g.shape[0]
+    _, axes = plt.subplots(ncols=n_graph, nrows=1)
+
+    for i in range(n_graph):
+        sns.heatmap(g[i], ax=axes[i], cbar=False, vmin=-1, vmax=1,
+                    cmap="Blues", xticklabels=False, yticklabels=False)
+        axes[i].set_title(f"G_T - {i}")
+        axes[i].set_aspect('equal', adjustable='box')
+
+    plt.savefig(os.path.join(path, 'adjacencies.png'))
+    plt.close()
+
+
+def plot_adjacency_w(w, path):
+    n_w = w.shape[1]
+    _, axes = plt.subplots(ncols=n_w, nrows=1)
+
+    for i in range(n_w):
+        sns.heatmap(w[:, i], ax=axes[i], cbar=False, vmin=-1, vmax=1,
+                    cmap="Blues", xticklabels=False, yticklabels=False)
+        axes[i].set_title(f"w_d={i}")
+        axes[i].set_aspect('equal', adjustable='box')
+
+    plt.savefig(os.path.join(path, 'ws.png'))
+    plt.close()
+
+def plot_x(x, path):
+    d = x.shape[1]
+    d_x = x.shape[2]
+
+    for i in range(d):
+        _, axes = plt.subplots(nrows=d_x, ncols=1)
+        for i_x in range(d_x):
+            axes[i_x].plot(x[:, i, i_x])
+        plt.savefig(os.path.join(path, f'x_{i}.png'))
+        plt.close()
+
+def plot_z(z, path):
+    d = z.shape[1]
+    k = z.shape[2]
+
+    for i in range(d):
+        _, axes = plt.subplots(nrows=k, ncols=1)
+        for i_k in range(k):
+            axes[i_k].plot(z[:, i, i_k])
+        plt.savefig(os.path.join(path, f'z_{i}.png'))
+        plt.close()
