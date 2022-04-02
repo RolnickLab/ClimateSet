aec148f43df747f3d93adc09dce841fbde0eb730
Philippe Brouillard <philippebrouillard@gmail.com>
2022-04-02 17:59:35 -0400

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+class Testing(unittest.TestCase):
+  def dummy_test(self):
+    a = True
+    b = True
+    self.assertEqual(a, b)
+    
+if __name__ == "__main__":
+  unittest.main()
