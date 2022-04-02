e9a4e78385143934999887b4c57c374c60ec4853
Philippe Brouillard <philippebrouillard@gmail.com>
2022-04-02 18:02:49 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,10 +1,12 @@
 import unittest
 
+
 class Testing(unittest.TestCase):
-  def dummy_test(self):
-    a = True
-    b = True
-    self.assertEqual(a, b)
-    
+    def dummy_test(self):
+        a = True
+        b = True
+        self.assertEqual(a, b)
+
+
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
