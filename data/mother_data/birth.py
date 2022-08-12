877a146308ab8b3994ccefdcf31338a92a89b3d7
liellnima <jkaltenborn@uos.de>
2022-08-12 18:50:26 -0400

    started to set up script, header, docs

--- data/mother_data/birth.py
+++ data/mother_data/birth.py
@@ -1,4 +1,7 @@
-# init file to establish first data basis
+# init script to establish first data basis
+
+from data_paths import RAW_DATA, PROCESSED_DATA, LOAD_DATA
+from mother_params import MOTHER_PARAMS
 
 # interacts with:
 # mother_params,
@@ -10,3 +13,71 @@
 
 # ATTENTION
 # during aggregation: take care of units and how they change (should be handled within raw_preprocesser)
+
+def birth(models: list[str], scenarios: list[str], years: list[int],
+        in_vars: list[str], out_vars: list[str], resolutions: tuple[int, int, int, int],
+        grid: str, aggregations: dict[str, str], interpolations: dict[str, str],
+        raw_path: Path, res_path: Path, load_path: Path):
+    """ Creates the initial climate data that is needed. Starts downloader,
+    makes initial preprocessing and default resolution processing. Data can
+    afterwards be loaded via Loader() or run.py. Prints if process was successful.
+
+    Parameters:
+        models (list<str>): String indicating which models should be used
+        scenarios (list<str>): String indicaitng which scenarios should be considered
+        years (list<int>): List of ints - the years for which the data is retrieved
+        in_vars (list<str>): List of model input variables that should be downlaoded
+        out_vars (list<str>): List of targets / output from models that should be downloaded
+        resolutions (tuple<int>): temporal, vertical, longitude and latitude resolution
+        grid (str): Which type of grid is used
+        agreggations (dict<str, str>): all in_vars and out_vars must be contained
+            in this dictionary. The value decodes which aggregation method should be used
+            in case this variable must be aggregated.
+        interpolations (dict<str, str>): Same as before, just interpolation
+        raw_path (Path): Path where raw data is downloaded into
+        res_path (Path): Path where the processed data is stored
+        load_path (Path): Here, the data is ready for being loaded
+    """
+    # DOWNLOAD DATA
+    print("Starting to download data ...")
+    # init downloader
+    # TODO
+
+    # execute downloader
+    # TODO
+    # TODO: catch error messages, take care of restarting the process?
+
+    print("... data was downloaded successfully.")
+
+    # PREPROCESS DATA
+    print("Starting to process the data ...")
+    # init preprocesser
+    # TODO
+
+    # execute preprocesser
+    # (might be different funcs)
+
+    print("... data was preprocessed successfully.")
+
+    # CREATE RIGHT RESOLUTIONS
+    print("Starting to get the right resolutions ...")
+    # init resolution processer
+
+    # do stuff
+
+    print("... data is now available in the right resolutions.")
+    print("\nClimate data is now available in {}.".format("TODO"))
+
+# params (dict): Dictionary containing parameters (stored in mother_params.py)
+#     such as resolutions, variables, grid type, years and aggregation type.
+# paths (dict): where the data is stored: raw data, preprocessed data and
+#     data that can be immediately loaded
+if __name__ == '__main__':
+    # load mother_params
+
+    # and put all of this into birth()
+    birth(**MOTHER_PARAMS,
+        raw_path = RAW_DATA,
+        res_path = PROCESSED_DATA,
+        load_path = LOAD_DATA
+    )
