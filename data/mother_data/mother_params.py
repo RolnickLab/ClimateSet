8591b7c695f19f8da1280144546033119cf1e68f
liellnima <jkaltenborn@uos.de>
2022-08-12 18:51:02 -0400

    added dict including all params

--- data/mother_data/mother_params.py
+++ data/mother_data/mother_params.py
@@ -1,12 +1,25 @@
+MOTHER_PARAMS = {
+    "models": MODELS,
+    "scenarios": SCENARIOS,
+    "years": YEARS,
+    "in_vars": IN_VARS,
+    "out_vars": OUT_VARS,
+    "resolutions": RESOLUTION,
+    "grid": GRID,
+    "aggregations": CHOSEN_AGGREGATIONS,
+    "interpolations": CHOSEN_INTERPOLATIONS,
+}
+
 ### DOWNLOADER PARAMS ##########################################################
 
 # resolution of the end-data-product
-RESOLUTION = []
+RESOLUTION = (TEMP_RES, VERT_RES, LON_RES, LAT_RES)
 
 # these resolutions are stored in RESOLUTION
-TEMP_RES
-VERT_RES
-LON_LAT_RES
+TEMP_RES = 0
+VERT_RES = 0
+LON_RES = 0
+LAT_RES = 0
 
 # list of years that are considered for the data
 YEARS = []
@@ -24,7 +37,7 @@ SCENARIOS = []
 MODELS = []
 
 # which type of grid
-GRID = []
+GRID = "grid"
 
 ### RAW PROCESSER PARAMS #######################################################
 # you will see after downloading
@@ -35,6 +48,7 @@ GRID = []
 # THIS must be moved somewhere else, because it's not static
 # tuple of "means" of preprocesser for each variable, e.g.
 # [("CO2", "mean"), ["CH4", "median"]
-CHOSEN_AGGREGATIONS = []
+CHOSEN_AGGREGATIONS = {}
 # TODO communicate to other persons which data structure etc you use here
-CHOSEN_INTERPOLATIONS = []
+CHOSEN_INTERPOLATIONS = {}
+# TODO create a fixed list for all vars: which aggregation and interpolation
