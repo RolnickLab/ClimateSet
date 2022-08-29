8ea83f98e421010de6b81018b7a387a05d9e6bd4
Charlie Lange <charlie.lange@posteo.de>
2022-08-29 17:10:59 -0400

    finished downloader

--- data/mother_data/utils/helper_funcs.py
+++ data/mother_data/utils/helper_funcs.py
@@ -1,6 +1,7 @@
 import subprocess
 import numpy as np
 
+
 def get_keys_from_value(d, val):
     keys = [k for k, v in d.items() if val in v]
     if keys:
@@ -9,17 +10,13 @@ def get_keys_from_value(d, val):
     return None
 
 
-def runcmd(cmd, verbose = False, *args, **kwargs):
+def runcmd(cmd, verbose=False, *args, **kwargs):
     """
     Run a bash command.
     """
 
     process = subprocess.Popen(
-        cmd,
-        stdout = subprocess.PIPE,
-        stderr = subprocess.PIPE,
-        text = True,
-        shell = True
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True
     )
     std_out, std_err = process.communicate()
     if verbose:
@@ -31,16 +28,16 @@ def get_MIP(experiment: str):
     """
     Return name of MIP group given the specific experiment name.
     """
-    if experiment == 'ssp245-covid':
-        return 'DAMIP'
-    elif experiment == 'ssp370-lowNTCF':
-        return 'AerChemMIP'
-    elif experiment.startswith('ssp'):
-        return 'ScenarioMIP'
-    elif experiment.startswith('hist-'):
-        return 'DAMIP'
+    if experiment == "ssp245-covid":
+        return "DAMIP"
+    elif experiment == "ssp370-lowNTCF":
+        return "AerChemMIP"
+    elif experiment.startswith("ssp"):
+        return "ScenarioMIP"
+    elif experiment.startswith("hist-"):
+        return "DAMIP"
     else:
-        return 'CMIP'
+        return "CMIP"
 
 
 def get_lowest_entry(availabe: [str], hierachy: [str]):
@@ -48,8 +45,8 @@ def get_lowest_entry(availabe: [str], hierachy: [str]):
     From a given list of available items return the lowest item according to a predefined hierachy.
     Used for e.g. finding the lowest available temporal resolution.
     """
-    index_list= np.where([i in availabe for i in hierachy])[0]
-    if len(index_list)==0:
+    index_list = np.where([i in availabe for i in hierachy])[0]
+    if len(index_list) == 0:
         return None
     else:
         return hierachy[index_list[0]]
