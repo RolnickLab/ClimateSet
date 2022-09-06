6653069bf5a04cc2701472ae2325c224ca1ddea7
charlotte.lange <charlotte.lange@login-4.server.mila.quebec>
2022-09-06 11:46:08 -0400

    readded env4

--- env4/bin/activate.fish
+++ env4/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/network/scratch/c/charlotte.lange/causalpaca/env4"
+set -gx VIRTUAL_ENV "/network/scratch/c/charlotte.lange/env4"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
