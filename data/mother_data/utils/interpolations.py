f06b653132e83085c6b549e38611c8e228135b51
liellnima <jkaltenborn@uos.de>
2022-10-18 17:21:34 -0400

    add interpolation versions

--- data/mother_data/utils/interpolations.py
+++ data/mother_data/utils/interpolations.py
@@ -2,19 +2,26 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
 
+import numpy as np
+
+
 # see data/mother_data/utils/aggregations.py
 # same thing, just other interpolation methods
 
 ### ABSTRACT CLASS #############################################################
 class Interpolation(ABC):
-    def __init__(self, var_path: Path, store_path: Path):
+    def __init__(self, var_path: Path, store_path: Path, another_var = np.array):
         """ Init method that is the same for all subclasses
         Parameters:
             var_path (pathlib.Path)
             store_path (pathlib.Path)
+            another_var (np.array)
         """
         # TODO
-        # self.var_path = var_path
+        self.var_path = var_path
+        self.store_path = store_path
+        self.new_res_var = None
+        self.another_var = another_var
         # self.store_path = store_path
         # self.new_res_var= None
         raise NotImplementedError
@@ -43,3 +50,19 @@ class Interpolation(ABC):
 # Splines
 # Inverse Distance weighted [new for me]
 # Kriging [new for me]
+
+class LinearInterpolation(Interpolation):
+    """ Make a linear interpolation
+    """
+    def interpolate(self):
+        raise NotImplementedError
+
+class NearestNeighborInterpolation(Interpolation):
+    """ Make a nearest neighbor interpolation
+    """
+    def interpolate(self):
+        raise NotImplementedError
+
+class PolynomialInterpolation(Interpolation):
+    """
+    """
