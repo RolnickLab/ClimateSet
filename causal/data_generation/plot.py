8c3644fed513348b0def9fcd5f16c9c8af6f43eb
kurowasan <philippebrouillard@gmail.com>
2022-12-14 17:42:27 -0800

    fix plot when d_x is large, just show the first timeseries

--- causal/data_generation/plot.py
+++ causal/data_generation/plot.py
@@ -49,31 +49,40 @@ def plot_adjacency_w(w: np.ndarray, path: str):
     plt.close()
 
 
-def plot_x(x: np.ndarray, path: str, t: int = 500):
-    """Plot the timeseries data X from 0 to t.
+def plot_x(x: np.ndarray, path: str, t: int = 500, d_max: int = 5, dx_max: int = 8):
+    """Plot the timeseries data X from T - t to T.
+    (where T is the total number of timesteps)
     Generate a figure for each feature.
 
     Args:
         x: an array containing the data X (shape: n x t x d x d_x)
         path: path where to save the generated figure
-        t: last timestep to include
+        t: show only the last t timesteps
+        d_max: total number of grid-locations to show
+        dx_max: total number of grid-locations to show
     """
     d = x.shape[2]
     d_x = x.shape[3]
     i_n = 0
 
     # if the timeserie is too short, plot it entirely
+    # TODO: change code if use multiple timeseries
     if t > x.shape[1]:
         t = x.shape[1]
+
+    if d > d_max:
+        d = d_max
+    if d_x > dx_max:
+        d_x = dx_max
 
     for i in range(d):
         _, axes = plt.subplots(nrows=d_x, ncols=1)
         if d_x == 1:
             # specific case when there is only one gridcell
-            axes.plot(x[i_n, :t, i, 0])
+            axes.plot(x[i_n, -t:, i, 0])
         else:
             for i_x in range(d_x):
-                axes[i_x].plot(x[i_n, :t, i, i_x])
+                axes[i_x].plot(x[i_n, -t:, i, i_x])
         plt.savefig(os.path.join(path, f'x_{i}.png'))
         plt.close()
 
