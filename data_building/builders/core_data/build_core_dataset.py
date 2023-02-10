89bb47114c9044b8123439d3564a49be0f77e43a
liellnima <julia.kaltenborn@mail.mcgill.ca>
2023-02-10 14:42:57 -0500

    update imports

--- data_building/builders/core_data/build_core_dataset.py
+++ data_building/builders/core_data/build_core_dataset.py
@@ -3,8 +3,8 @@ from pathlib import Path
 from typing import List, Tuple, Dict
 
 #from data_generation.generators.mother_data import *
-from data_generation.parameters.mother_params import MOTHER_PARAMS
-from data_generation.parameters.data_paths import RAW_DATA, PROCESSED_DATA, LOAD_DATA
+from data_building.parameters.esm_params import MOTHER_PARAMS
+from data_building.parameters.data_paths import RAW_DATA, PROCESSED_DATA, LOAD_DATA
 
 # interacts with:
 # mother_params,
