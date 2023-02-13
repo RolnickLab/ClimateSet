2d6bf2c1872829f15fe702034dce8f30851e2f87
liellnima <julia.kaltenborn@mail.mcgill.ca>
2023-02-13 14:28:18 -0500

    update setup.py

--- setup.sh
+++ setup.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
+# TODO if else environment
+# if module load python/3.9 is possible -> you are on a cluster
+# if not -> presumably you are not on cluster
+
+
 # 1. Load modules if available
-module load python/3.7
+module load python/3.9
+# module load python/3.7
 #module load cdo # TODO if else
 
 # 2. Install CDO if not available as module
 # TODO how to do this on the cluster? without sudo?
 sudo apt-get install libnetcdf-dev libhdf5-dev
 sudo apt-get install cdo
+# TODO check if this already exists before installing it!!
 
 # 3. Create venv
-if [ -a env37/bin/activate ]; then
-    source env37/bin/activate
+if [ -a env_data/bin/activate ]; then
+    source env_data/bin/activate
 
 else
-    echo "env37 is created now"
-    python3.7 -m venv env37
-    source env37/bin/activate
-    python3.7 -m pip install --upgrade pip
-    pip3.7 install wheel setuptools
+    echo "env_data is created now"
+    python3.9 -m venv env_data
+    source env_data/bin/activate
+    python3.9 -m pip install --upgrade pip
+    pip3.9 install wheel setuptools
 
     # 4. Install requirements.txt if it exists
 
-    if [ -a requirements2.txt ]; then
-        pip3.7 install -r requirements2.txt
+    if [ -a requirements_data.txt ]; then
+        pip3.9 install -r requirements_data.txt
     fi
 
 fi
