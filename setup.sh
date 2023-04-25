ab9269cf9dfd145b3c7f1e9db9fba479632a03d8
liellnima <julia.kaltenborn@mail.mcgill.ca>
2023-04-24 21:36:30 -0400

    adapt setup file for different cases

--- setup.sh
+++ setup.sh
@@ -1,36 +1,68 @@
 #!/bin/bash
 
-# TODO if else environment
-# if module load python/3.9 is possible -> you are on a cluster
-# if not -> presumably you are not on cluster
+# TODO conda environment case
 
+venv='false'
+cluster='false'
 
-# 1. Load modules if available
-module load python/3.9
-# module load python/3.7
-#module load cdo # TODO if else
+while getopts vc flag; do
+    case "${flag}" in
+      v) venv='true' ;;
+      c) cluster='true' ;;
+    esac
+done
 
-# 2. Install CDO if not available as module
-# TODO how to do this on the cluster? without sudo?
-sudo apt-get install libnetcdf-dev libhdf5-dev
-sudo apt-get install cdo
-# TODO check if this already exists before installing it!!
-
-# 3. Create venv
-if [ -a env_data/bin/activate ]; then
-    source env_data/bin/activate
-
+if $cluster; then
+  module load python/3.9 || echo "Python module cannot be loaded."
+  if $venv; then
+    module load cdo/2.0.5 || echo "CDO module cannot be loaded. Consider uding a conda environment instead"
+  fi
 else
-    echo "env_data is created now"
-    python3.9 -m venv env_data
-    source env_data/bin/activate
-    python3.9 -m pip install --upgrade pip
-    pip3.9 install wheel setuptools
+  # install cdo in case we are local and use venv
+  if $venv; then
+    sudo apt-get install libnetcdf-dev libhdf5-dev
+    sudo apt-get install cdo
+  fi
+fi
 
-    # 4. Install requirements.txt if it exists
+# Install and activate environments
 
-    if [ -a requirements_data.txt ]; then
-        pip3.9 install -r requirements_data.txt
+# VENV case
+if $venv; then
+  if [ -a env_data/bin/activate ]; then
+      source env_data/bin/activate
+
+  else
+      echo "env_data is created now"
+      python3.9 -m venv env_data
+      source env_data/bin/activate
+      python3.9 -m pip install --upgrade pip
+      pip3.9 install wheel setuptools
+
+      # Install requirements.txt if it exists
+      if [ -a requirements_data.txt ]; then
+          pip3.9 install -r requirements_data.txt
+      else
+          echo "Requirements file missing"
+      fi
+
+  fi
+
+# CONDA case
+else
+    # if conda environment does not exists already
+    echo "TODO this is broken - you have to install conda yourself right now"
+    exit 1
+    if conda info --envs | grep -q conda_env_data; then
+        source activate conda_env_data
+    else
+        conda create -n conda_env_data python=3.9
+        source activate conda_env_data
+        if [ -a requirements_data.txt ]; then
+            pip install -r requirements_data.txt
+            conda install -c conda-forge cdo
+        else
+            echo "Requirements file missing"
+        fi
     fi
-
 fi
