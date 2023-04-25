6b606602f84f622cfe959ab0d7c6875b5ca8bbaf
liellnima <julia.kaltenborn@mail.mcgill.ca>
2023-04-24 21:35:31 -0400

    adapt datapaths for compute canada

--- data_building/parameters/data_paths.py
+++ data_building/parameters/data_paths.py
@@ -3,13 +3,16 @@ from pathlib import Path
 from data_building.utils.helper_funcs import get_project_root
 
 # where the raw data is downloaded, stored and deleted after preprocessing
-RAW_DATA = Path("/network/scratch/j/julia.kaltenborn/data/raw/") # always deleted
+RAW_DATA = Path("/home/jkalt/projects/def-drolnick/jkalt/data/raw/") # always deleted
+#RAW_DATA = Path("/network/scratch/j/julia.kaltenborn/data/raw/") # always deleted
 
 # this data is stored and can be used for new user requests
-PROCESSED_DATA = Path("/network/scratch/j/julia.kaltenborn/data/processed/") # never deleted
+PROCESSED_DATA = Path("/home/jkalt/projects/def-drolnick/jkalt/data/processed/") # never deleted
+#PROCESSED_DATA = Path("/network/scratch/j/julia.kaltenborn/data/processed/") # never deleted
 
 # this data can be used within the data loader
-LOAD_DATA = Path("/network/scratch/j/julia.kaltenborn/data/load/") # deleted if necessary, can be recreated from processed_data
+LOAD_DATA = Path("/home/jkalt/projects/def-drolnick/jkalt/data/load/") # deleted if necessary
+#LOAD_DATA = Path("/network/scratch/j/julia.kaltenborn/data/load/") # deleted if necessary, can be recreated from processed_data
 
 # root of the project
 ROOT_DIR = get_project_root()
