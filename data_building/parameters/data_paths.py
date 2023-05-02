29ca6174150985d164a16a2b05ed56de43b87aef
liellnima <julia.kaltenborn@mail.mcgill.ca>
2023-05-02 00:52:03 -0400

    add path to data that can be used for testing processing functions

--- data_building/parameters/data_paths.py
+++ data_building/parameters/data_paths.py
@@ -9,3 +9,5 @@ PROCESSED_DATA = Path("/network/scratch/j/julia.kaltenborn/data/processed/") # n
 
 # this data can be used within the data loader
 LOAD_DATA = Path("/network/scratch/j/julia.kaltenborn/data/load/") # deleted if necessary, can be recreated from processed_data
+
+TEST_CASES = Path("/home/julia/Documents/Master/CausalSuperEmulator/data/test_cases/")
