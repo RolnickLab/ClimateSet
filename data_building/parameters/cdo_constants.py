f2359b09f5a9bf8b8ebb39d23895ca5ea5358fbb
liellnima <julia.kaltenborn@mail.mcgill.ca>
2023-05-02 00:51:20 -0400

    add file to store available cdo options

---
+++ data_building/parameters/cdo_constants.py
@@ -0,0 +1,22 @@
+SPAT_INTERPOLATION_ALGS = [
+    "remapbil",
+    "remapbic",
+    "remapnn",
+    "remapdis",
+    "remapycon",
+    "remapcon",
+    "remapcon2",
+    "remaplaf",
+]
+
+SPAT_AGGREGATION_ALGS = [
+
+]
+
+TEMP_INTERPOLATION_ALGS = [
+
+]
+
+TEMP_AGGREGATION_ALGS = [
+
+]
