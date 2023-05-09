2e5cc6f989aad182e8fc6fe332c8b0246e856f38
liellnima <julia.kaltenborn@mail.mcgill.ca>
2023-05-09 07:50:49 -0400

    add temporal remapping, add dict for weight creation

--- data_building/parameters/cdo_constants.py
+++ data_building/parameters/cdo_constants.py
@@ -1,22 +1,49 @@
-SPAT_INTERPOLATION_ALGS = [
+SPAT_REMAPPING_ALGS = [
+    # simple operator
+    "remap",
+    # specific operators
     "remapbil",
     "remapbic",
     "remapnn",
     "remapdis",
-    "remapycon",
     "remapcon",
     "remapcon2",
     "remaplaf",
 ]
 
-SPAT_AGGREGATION_ALGS = [
-
-]
+# creating remap weights
+SPAT_REMAPPING_WEIGHTS = {
+    "remapbil": "genbil",
+    "remapbic": "genbic",
+    "remapnn": "gennn",
+    "remapdis": "gendis",
+    "remapcon": "gencon",
+    "remapcon2": "gencon2",
+    "remaplaf": "genlaf",
+}
 
 TEMP_INTERPOLATION_ALGS = [
+    "inttime",
+    "intntime"
+    "intyear",
 
 ]
 
 TEMP_AGGREGATION_ALGS = [
 
+]
+
+# for later usage
+LEVS_INTERPOLATION_ALGS = [
+    "remapeta"
+    "ml2pl",
+    "ml2hl",
+    "ap2pl",
+    "gh2hl"
+]
+
+VAR3D_INTERPOLATION_ALGS = [
+    "intlevel",
+    "intlevel3d",
+    "intlevelx3d"
 ]
