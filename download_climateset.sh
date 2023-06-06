f9cef42b5cd3b053f1b23129343f8f55a62b9f39
Venkatesh <venka97@gmail.com>
2023-06-07 01:32:28 +0530

    Extract tar files and delete them progressively

--- download_climateset.sh
+++ download_climateset.sh
@@ -16,8 +16,19 @@ for x in AWI-CM-1-1-MR.tar.gz BCC-CSM2-MR.tar.gz CAMS-CSM1-0.tar.gz CAS-ESM2-0.t
 
 #curl https://object-arbutus.cloud.computecanada.ca/causalpaca/outputs/NorESM2-LM.tar.gz --output ${data_dir}/outputs/NorESM2-LM.tar.gz
 
-echo "Done."
+echo "Done. Finished downloading the compressed files, now extracting!"
 
+cd ${data_dir}/inputs
+for x in `ls |grep .gz`;
+do tar -xzf $x;
+do rm -f $x;
+done;
+
+cd ../../${data_dir}/outputs
+for x in `ls |grep .gz`;
+do tar -xzf $x;
+do rm -f $x;
+done;
 
 
 echo "Done. Finished downloading Climateset data! :)"
