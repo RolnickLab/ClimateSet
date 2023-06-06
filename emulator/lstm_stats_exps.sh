742aa59a76cb705c3f21818cfdc8082b73788db7
Venka97 <venka97@gmail.com>
2023-06-06 17:29:24 -0400

    Add more configs

---
+++ emulator/lstm_stats_exps.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --job-name=causalpaca_lstm
+#SBATCH --output=nohup.out
+#SBATCH --error=error.out
+#SBATCH --ntasks=1
+#SBATCH --time=5:00:00
+#SBATCH --mem=48Gb
+#SBATCH --gres=gpu:rtx8000:1
+#SBATCH --partition=main
+#SBATCH -c 4
+
+module add python/3.10
+source $HOME/env_emulator/bin/activate
+
+python run.py experiment=single_emulator/convlstm/AWI-CM-1-1-MR_convlstm_tas+pr_run-01.yaml seed=3423
+python run.py experiment=single_emulator/convlstm/AWI-CM-1-1-MR_convlstm_tas+pr_run-01.yaml seed=22201
+python run.py experiment=single_emulator/convlstm/AWI-CM-1-1-MR_convlstm_tas+pr_run-01.yaml seed=7
+
+python run.py experiment=single_emulator/convlstm/FGOALS-f3-L_convlstm_tas+pr_run-01.yaml seed=3423
+python run.py experiment=single_emulator/convlstm/AWI-CM-1-1-MR_convlstm_tas+pr_run-01.yaml seed=22201
+python run.py experiment=single_emulator/convlstm/AWI-CM-1-1-MR_convlstm_tas+pr_run-01.yaml seed=7
+
+python run.py experiment=single_emulator/convlstm/AWI-CM-1-1-MR_convlstm_tas+pr_run-01.yaml seed=3423
+python run.py experiment=single_emulator/convlstm/AWI-CM-1-1-MR_convlstm_tas+pr_run-01.yaml seed=22201
+python run.py experiment=single_emulator/convlstm/AWI-CM-1-1-MR_convlstm_tas+pr_run-01.yaml seed=7
+
+python run.py experiment=single_emulator/convlstm/AWI-CM-1-1-MR_convlstm_tas+pr_run-01.yaml seed=3423
+python run.py experiment=single_emulator/convlstm/AWI-CM-1-1-MR_convlstm_tas+pr_run-01.yaml seed=22201
+python run.py experiment=single_emulator/convlstm/AWI-CM-1-1-MR_convlstm_tas+pr_run-01.yaml seed=7
+
+python run.py experiment=single_emulator/convlstm/AWI-CM-1-1-MR_convlstm_tas+pr_run-01.yaml seed=3423
+python run.py experiment=single_emulator/convlstm/AWI-CM-1-1-MR_convlstm_tas+pr_run-01.yaml seed=22201
+python run.py experiment=single_emulator/convlstm/AWI-CM-1-1-MR_convlstm_tas+pr_run-01.yaml seed=7
+
+python run.py experiment=single_emulator/convlstm/AWI-CM-1-1-MR_convlstm_tas+pr_run-01.yaml seed=3423
+python run.py experiment=single_emulator/convlstm/AWI-CM-1-1-MR_convlstm_tas+pr_run-01.yaml seed=22201
+python run.py experiment=single_emulator/convlstm/AWI-CM-1-1-MR_convlstm_tas+pr_run-01.yaml seed=7
+
---
+++ emulator/start_exps.sh
@@ -0,0 +1,4 @@
+python run.py experiment=single_emulator/unet/CESM2-WACCM_unet_tas+pr_run-01.yaml seed=3423
+python run.py experiment=single_emulator/unet/CESM2-WACCM_unet_tas+pr_run-01.yaml seed=22201
+python run.py experiment=single_emulator/unet/CESM2-WACCM_unet_tas+pr_run-01.yaml seed=7
+
