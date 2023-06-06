bafb40b7dcf376d5b7b4fee1c67ca317ffb109c2
Venka97 <venka97@gmail.com>
2023-06-06 11:54:36 -0400

    Update fire stuff

--- emulator/src/data/constants.py
+++ emulator/src/data/constants.py
@@ -16,6 +16,21 @@ SEQ_LEN_MAPPING ={
 
 DATA_DIR = '/home/mila/v/venkatesh.ramesh/scratch/causal_data' # MILA cluster
 # Model : (historical_obe_files, future_obe_files)
+
+#Available models 
+AVAILABLE_MODELS = ["CESM2-WACCM": ("no-fires", "no-fires" ),
+    "CNRM-ESM2-1": ("anthro-fires", "anthro-fires"),
+    "CMCC-ESM2": ("no-fires", "no-fires" ),
+    "EC-Earth3-Veg": ("anthro-fires", "anthro-fires"),
+    "EC-Earth3-Veg-LR": ("anthro-fires", "anthro-fires"),
+    "MPI-ESM1-2-LR": ("anthro-fires", "anthro-fires"),
+    "NorESM2-LM": ("no-fires", "no-fires" ),
+    "NorESM2-MM": ("no-fires", "no-fires" ),
+    "GFDL-ESM4": ("no-fires", "no-fires" ),
+    "TaiESM1": ("anthro-fires", "all-fires"),
+    "CESM2": ("anthro-fires", "all-fires"),
+    "MRI-ESM-2.0": ("anthro-fires", "all-fires")
+
 OPENBURNING_MODEL_MAPPING = {
     "other" : ("anthro-fires", "anthro-fires"),
     "CESM2-WACCM": ("no-fires", "no-fires" ),
