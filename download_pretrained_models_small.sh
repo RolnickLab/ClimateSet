76e092d9f6d061a53269f9b186acc41ef988fa07
Charlie Lange <charlie.lange@posteo.de>
2024-02-02 17:27:19 +0100

    update pretrained models

--- download_pretrained_models_small.sh
+++ download_pretrained_models_small.sh
@@ -1,10 +1,26 @@
+# Uncomment unwanted experiments / models
+# Be aware that the single emulator experiment folder containing ClimaX checkpoints is quite large (13Gb) and may take some time
+declare -a experiments=("single_emulator" "finetuning_emulator" "super_emulator" )
+declare -a models=("ClimaX_frozen" "ClimaX"  "CNN_LSTM_ClimateBench" "Unet")
 
 echo "Downloading pretrained models' checkpoints..."
-curl https://object-arbutus.cloud.computecanada.ca/causalpaca/pretrained_models_small.tar.gz --output pretrained_models_small.tar.gz
 
-echo "Done. Finished downloading the compressed files, now extracting!"
+for exp in "${experiments[@]}"; do
+    for m in "${models[@]}"; do
+        path="pretrained_models/"$exp"/"$m""
+        echo "Downloading "$exp" "$m""
+        file=""$path".zip"
+        link="https://object-arbutus.cloud.computecanada.ca/causalpaca/"$file"" 
+        curl $link --output $file  --create-dirs # bild correct string
+        # unpack
+        unzip $file -d $path
+        # remove zip
+        rm -f $file
+        echo "Finished "$exp" "$m""
 
-tar -xzf "pretrained_models_small.gz"; rm -f "pretrained_models_small.gz";
-
+    done
+done
 
 echo "Done. Finished downloading pretrained models. :)"
+
+
