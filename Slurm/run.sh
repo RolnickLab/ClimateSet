1a9045b1b7039b19b35ce058c14c2d5a85a1ff9a
Felix-Andreas Nahrstedt <contactfelixnahrstedt@gmail.com>
2024-04-16 16:39:05 -0400

    update run.sh to run all models

--- Slurm/run.sh
+++ Slurm/run.sh
@@ -10,13 +10,13 @@ while true; do
 done
 
 
-# sbatch Slurm/climax
-# sbatch Slurm/climax_36
-# sbatch Slurm/climax_frozen
-# sbatch Slurm/climax_frozen_36
+sbatch Slurm/climax
+sbatch Slurm/climax_36
+sbatch Slurm/climax_frozen
+sbatch Slurm/climax_frozen_36
 sbatch Slurm/convlstm
-# sbatch Slurm/convlstm_36
-# sbatch Slurm/unet
-# sbatch Slurm/unet_36
+sbatch Slurm/convlstm_36
+sbatch Slurm/unet
+sbatch Slurm/unet_36
 
 echo "Success - Go sleep and see your results in the morning!"
