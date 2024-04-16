129ec30df2ace4a7e24a89133d9f77ef74baf982
Felix-Andreas Nahrstedt <contactfelixnahrstedt@gmail.com>
2024-04-16 11:10:56 -0400

    A100 Branch wandb tags update

--- Slurm_A100/run.sh
+++ Slurm_A100/run.sh
@@ -10,13 +10,13 @@ while true; do
 done
 
 
-sbatch Slurm/climax
-sbatch Slurm/climax_36
-sbatch Slurm/climax_frozen
-sbatch Slurm/climax_frozen_36
-sbatch Slurm/convlstm
-sbatch Slurm/convlstm_36
-sbatch Slurm/unet
-sbatch Slurm/unet_36
+sbatch Slurm_A100/climax
+sbatch Slurm_A100/climax_36
+sbatch Slurm_A100/climax_frozen
+sbatch Slurm_A100/climax_frozen_36
+sbatch Slurm_A100/convlstm
+sbatch Slurm_A100/convlstm_36
+sbatch Slurm_A100/unet
+sbatch Slurm_A100/unet_36
 
 echo "Success - Go sleep and see your results in the morning!"
