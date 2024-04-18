cc031322bc19b39dc7522caa2f3055460d565600
Felix-Andreas Nahrstedt <contactfelixnahrstedt@gmail.com>
2024-04-18 09:51:52 -0400

    Batchsize testing unifying scripts run.sh

---
+++ Slurm_A100/BatchSize/run.sh
@@ -0,0 +1,19 @@
+pwd
+
+while true; do
+    read -p "Do you wish to remove old files? " yn
+    case $yn in
+        [Yy]* ) rm -f ../Slurm/{*,.*}; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
+sbatch Slurm_A100/BatchSize/convlstm_4
+sbatch Slurm_A100/BatchSize/convlstm_8
+sbatch Slurm_A100/BatchSize/convlstm_16
+sbatch Slurm_A100/BatchSize/convlstm_32
+
+
+echo "Success - Go sleep and see your results in the morning!"
