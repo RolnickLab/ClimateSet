7614b6bca1875be8164efdd84c982de3e9599b81
Felix-Andreas Nahrstedt <contactfelixnahrstedt@gmail.com>
2024-04-18 09:57:07 -0400

    FIX group changed for batchsize scripts

--- Slurm_A100/BatchSize/run.sh
+++ Slurm_A100/BatchSize/run.sh
@@ -11,9 +11,9 @@ done
 
 
 sbatch Slurm_A100/BatchSize/convlstm_4
-sbatch Slurm_A100/BatchSize/convlstm_8
+# sbatch Slurm_A100/BatchSize/convlstm_8
 sbatch Slurm_A100/BatchSize/convlstm_16
-sbatch Slurm_A100/BatchSize/convlstm_32
+# sbatch Slurm_A100/BatchSize/convlstm_32
 
 
 echo "Success - Go sleep and see your results in the morning!"
