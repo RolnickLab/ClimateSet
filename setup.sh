d1f129113bd46aa262bc5003ca0e73a6936adfd1
liellnima <julia.kaltenborn@mail.mcgill.ca>
2025-06-03 19:24:11 +0200

    update setup for poetry and windows compatability

--- setup.sh
+++ setup.sh
@@ -1,68 +1,95 @@
 #!/bin/bash
+# Copy: 
+# source env_emulator/bin/activate 
+# module load python/3.10
 
-# TODO conda environment case
 
-venv='false'
-cluster='false'
+# Default values for flags
+run_python=false
+run_bash=false
+run_checkpoints=false
+no_cluster=false
+on_windows=false
 
-while getopts vc flag; do
-    case "${flag}" in
-      v) venv='true' ;;
-      c) cluster='true' ;;
-    esac
+# Parse command line options
+while getopts pbcnh opt; do
+  case ${opt} in
+    p )
+      run_python=true
+      ;;
+    b )
+      run_bash=true
+      ;;
+    c )
+      run_checkpoints=true
+      ;;
+    n ) 
+      no_cluster=true
+      ;;
+    w )
+      on_windows=true
+      ;;
+    h )
+      echo "Usage: $0 [-p] [-b] [-c] [-n] [-w]"
+      echo "Options:"
+      echo "  -p: Run python download_climateset.py"
+      echo "  -b: Run bash download_climateset.sh if you are within Canada instead of -p"
+      echo "  -c: Run bash download_climax_checkpoints.sh"
+      echo "  -n: no access to the mila cluster"
+      echo "  -w: On a Windows System"
+
+      exit 0
+      ;;
+    \? )
+      echo "Invalid option: $OPTARG. Use -h for help." 1>&2
+      exit 1
+      ;;
+  esac
 done
+shift $((OPTIND -1))
 
-if $cluster; then
-  module load python/3.9 || echo "Python module cannot be loaded."
-  if $venv; then
-    module load cdo/2.0.5 || echo "CDO module cannot be loaded. Consider uding a conda environment instead"
-  fi
-else
-  # install cdo in case we are local and use venv
-  if $venv; then
-    sudo apt-get install libnetcdf-dev libhdf5-dev
-    sudo apt-get install cdo
-  fi
+# Set HYDRA_FULL_ERROR environment variable
+export HYDRA_FULL_ERROR=1
+
+if [ "$no_cluster" = false ]; then
+  # Load Python 3.10 module
+  module load python/3.10 || { echo "Python module cannot be loaded."; exit 1; }
+  module load libffi
+  # Set PYTHONPATH to current directory
+  # export PYTHONPATH=$(pwd)
 fi
 
-# Install and activate environments
-
-# VENV case
-if $venv; then
-  if [ -a env_data/bin/activate ]; then
-      source env_data/bin/activate
-
-  else
-      echo "env_data is created now"
-      python -m venv env_data
-      source env_data/bin/activate
-      python -m pip install --upgrade pip
-      pip install wheel setuptools
-
-      # Install requirements.txt if it exists
-      if [ -a requirements_data.txt ]; then
-          pip install -r requirements_data.txt
-      else
-          echo "Requirements file missing"
-      fi
-
-  fi
-
-# CONDA case
-else
-    # if conda environment does not exists already
-    echo "TODO this is broken - you have to install conda yourself right now"
-    exit 1
-    if conda info --envs | grep -q conda_env_data; then
-        source activate conda_env_data
-    else
-        conda create -n conda_env_data python=3.9
-        source activate conda_env_data
-        if [ -a requirements_data.txt ]; then
-            pip install -r requirements_data.txt
-            conda install -c conda-forge cdo
-        else
-            echo "Requirements file missing"
-        fi
-    fi
+if [ "$no_cluster" = true ]; then 
+  pip install poetry
 fi
+
+# Run Python script if -p flag is set
+if [ "$run_python" = true ]; then
+  python download_climateset.py || { echo "Failed to run download_climateset.py"; exit 1; }
+fi
+
+# Run Bash script if -b flag is set
+if [ "$run_bash" = true ]; then
+  bash download_climateset.sh || { echo "Failed to run download_climateset.sh"; exit 1; }
+fi
+
+# Run additional Bash script if -c flag is set
+if [ "$run_checkpoints" = true ]; then
+  bash download_climax_checkpoints.sh || { echo "Failed to run download_climax_checkpoints.sh"; exit 1; }
+fi
+
+# Check if env_emulator folder exists
+if [ ! -d "env_emulator" ]; then
+    # Create a virtual environment
+    python -m venv env_emulator || { echo "Failed to create virtual environment."; exit 1; }
+fi
+
+# Activate the virtual environment
+if [ "$on_windows" = false ]; then
+    source env_emulator/bin/activate || { echo "Failed to activate virtual environment."; exit 1; }
+else 
+    env_emulator/Scripts/activate || { echo "Failed to activate virtual environment."; exit 1; }
+fi 
+
+# Install the emulator package in editable mode 
+poetry install --all-groups || { echo "Failed to install emulator package."; exit 1; }
