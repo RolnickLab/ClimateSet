5d79bc4057decd5ab8a1c229859048ccb9c31294
liellnima <julia.kaltenborn@mail.mcgill.ca>
2025-07-07 09:53:53 -0400

    specify libffi version

--- setup.sh
+++ setup.sh
@@ -56,7 +56,7 @@ export HYDRA_FULL_ERROR=1
 if [ "$on_cluster" = true ]; then
   # Load Python 3.10 module
   module load python/3.10 || { echo "Python module cannot be loaded."; exit 1; }
-  module load libffi
+  module load libffi/3.2.1 || { echo "libffi module cannot be loaded."; exit 1; }
   # Set PYTHONPATH to current directory
   # export PYTHONPATH=$(pwd)
   # on the cluster we already have poetry
@@ -90,6 +90,5 @@ if [ ! -d "env_emulator" ]; then
         env_emulator/Scripts/activate || { echo "Failed to activate virtual environment."; exit 1; }
     fi 
     # Install the emulator package in editable mode 
-    export PYTHONPATH=$(pwd)
     poetry install --all-groups || { echo "Failed to install emulator package."; exit 1; }
 fi
